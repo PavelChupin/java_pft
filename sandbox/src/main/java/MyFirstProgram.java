bf570d58f00cf2d71ef4e08da4c5f47f92b8a09f
PavelChupin <pavel.chupin@gmail.com>
2017-02-11 11:16:07 +0700

    Первая программа

--- sandbox/src/main/java/MyFirstProgram.java
+++ sandbox/src/main/java/MyFirstProgram.java
@@ -1,6 +1,8 @@
 public class MyFirstProgram
 {
 public static void main(String[] args){
-System.out.println("Hello, world!"); 
+for(int i = 0;i<3;i++) {
+    System.out.println("Start learning");
+}
 }
 }

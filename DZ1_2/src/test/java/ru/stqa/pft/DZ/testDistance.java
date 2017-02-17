fae47c3774b993fefbf76e4635238951828f7d5d
PavelChupin <pavel.chupin@gmail.com>
2017-02-17 13:20:41 +0700

    Подключен TestNG

--- DZ1_2/src/test/java/ru/stqa/pft/DZ/testDistance.java
+++ DZ1_2/src/test/java/ru/stqa/pft/DZ/testDistance.java
@@ -1,11 +1,19 @@
 package ru.stqa.pft.DZ;
 
+import org.testng.annotations.Test;
+
 /**
  * Created by Summoner on 17.02.2017.
  */
-public class testDistance {
-    Point p1 = new Point(6,8);
-    Point p2 = new Point(6,8);
+
+
+public class TestDistance {
+
+    @Test
+    public void testDistance() {
+        Point p1 = new Point(6, 8);
+        Point p2 = new Point(6, 8);
+    }
 
 
 }

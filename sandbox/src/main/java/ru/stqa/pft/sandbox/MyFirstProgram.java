a009f465dcfe7d803bab05e38463262f0a5ef4e0
PavelChupin <pavel.chupin@gmail.com>
2017-02-25 19:59:40 +0700

    Пример из Лекции

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -7,10 +7,10 @@ public class MyFirstProgram {
         hello("Alexei");
 
         Square s = new Square(5);
-        System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
+        System.out.println("??????? ???????? ?? ???????? " + s.l + " = " + s.area());
 
         Rectangle r = new Rectangle(4,6);
-        System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
+        System.out.println("??????? ?????????????? ?? ????????? " + r.a + " ? " + r.b + " = " + r.area());
     }
 
     public static void hello(String somebody) {

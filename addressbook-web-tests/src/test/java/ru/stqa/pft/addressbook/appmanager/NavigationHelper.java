ca6474921259f83c3859acb54e6bc6262969cc32
PavelChupin <pavel.chupin@gmail.com>
2017-02-26 22:41:31 +0700

    Выполнена оптимизация кода

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -0,0 +1,19 @@
+package ru.stqa.pft.addressbook.appmanager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+/**
+ * Created by Summoner on 26.02.2017.
+ */
+public class NavigationHelper extends HelperBase{
+
+
+    public NavigationHelper(FirefoxDriver wd) {
+        super(wd);
+    }
+
+    public void gotoGroupPage() {
+        click(By.linkText("groups"));
+    }
+}
